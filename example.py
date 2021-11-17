4018c6481d498fa97b73af434c0ffe273e92768a
Faisal Lalani <flalani@ip-10-0-2-160.ap-south-1.compute.internal>
2021-11-17 16:42:12 +0000

    updated example.py to re-add deleted lines

--- example.py
+++ example.py
@@ -18,6 +18,18 @@ import pandas as pd
 DATA_FILE = 'example_data.csv'
 QUESTIONS = ['cough', 'fever']
 
+def compute_mma(data):
+    # Compute MMA outlier scores.
+    (mma_scores, _) = outlierdetect.run_mma(data, 'interviewer_id', QUESTIONS)
+    print("\nMMA outlier scores")
+    print_scores(mma_scores)
+
+def compute_sva(data):
+    # Compute SVA outlier scores.
+    (sva_scores, _) = outlierdetect.run_sva(data, 'interviewer_id', QUESTIONS)
+    print("\nSVA outlier scores")
+    print_scores(sva_scores)
+
 def print_scores(scores):
     for interviewer in scores.keys():
 
@@ -39,8 +51,10 @@ def print_scores(scores):
 
 if __name__ == '__main__':
     data = pd.read_csv(DATA_FILE)  # Uncomment to load as pandas.DataFrame.
+    # data = mlab.csv2rec(DATA_FILE)  # Uncomment to load as numpy.recarray.
 
-    # Compute MMA outlier scores.
-    (mma_scores, _) = outlierdetect.run_mma(data, 'interviewer_id', QUESTIONS)
-    print("\nMMA outlier scores")
-    print_scores(mma_scores)
+    # Compute MMA outlier scores.  Will work only if scipy is installed.
+    if hasattr(outlierdetect, 'run_mma'):
+        compute_mma(data)
+    
+    # compute_sva(data)
